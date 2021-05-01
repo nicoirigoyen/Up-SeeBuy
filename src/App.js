7225c5576a1344bdc5497371383f6cf7cad42d90
nicoirigoyen <nico.irigoyen.terre@hotmail.com>
2021-05-01 19:33:15 -0300

    prueba rama

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>HOLA</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
+        
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
