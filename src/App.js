c46121a27e738e8ca7120ba63f6561b8a119d2cc
Martin Pinillos Pinto <martinpinillos@me.com>
2021-06-06 22:57:32 -0300

    commit de prueba Martin

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-        
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
